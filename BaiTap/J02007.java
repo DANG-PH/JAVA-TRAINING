0496e24a4f43178cd79bed54b1a7afb0031e891a
DANG-PH <dangph.ptit@gmail.com>
2025-08-21 20:31:49 +0700

    Làm bài J02007 theo cấu trúc OOP

---
+++ BaiTap/J02007.java
@@ -0,0 +1,75 @@
+import java.util.*;
+
+class XetListSoNguyen {
+    private List<Integer> list;
+
+    public XetListSoNguyen(List<Integer> list) {
+        this.list = list;
+    }
+
+    public List<SoLanXuatHienMotSo> soLanXuatHienPhanTu() {
+        List<SoLanXuatHienMotSo> cacCap = new ArrayList<>();
+        Map<Integer, Integer> map = new LinkedHashMap<>();
+
+        for (int i = 0; i < list.size(); i++) {
+            if (!map.containsKey(list.get(i))) {
+                map.put(list.get(i), 0);
+            }
+            map.put(list.get(i),map.get(list.get(i))+1);
+        }
+        for (int key : map.keySet()) {
+            cacCap.add(new SoLanXuatHienMotSo(key,map.get(key)));
+        }
+        return cacCap;
+    }
+}
+
+class SoLanXuatHienMotSo {
+    private int soCanXet;
+    private int lanXuatHien;
+
+    public int getSoCanXet() {
+        return soCanXet;
+    }
+
+    public int getLanXuatHien() {
+        return lanXuatHien;
+    }
+
+    public SoLanXuatHienMotSo(int soCanXet, int lanXuatHien) {
+        this.soCanXet = soCanXet;
+        this.lanXuatHien = lanXuatHien;
+    }
+}
+
+
+class XuLyBaiToanJ02007 {
+    public void thucThi(Scanner sc) {
+        int boTest = sc.nextInt();
+        for (int i = 0; i < boTest; i++) {
+            int n = sc.nextInt();
+            List<Integer> list = new ArrayList<>();
+            while (n-- > 0) {
+                list.add(sc.nextInt());
+            }
+            XetListSoNguyen xetListSoNguyen = new XetListSoNguyen(list);
+            List<SoLanXuatHienMotSo> listCap = xetListSoNguyen.soLanXuatHienPhanTu();
+            inKetQua(listCap, i);
+        }
+    }
+
+    private void inKetQua(List<SoLanXuatHienMotSo> listCap, int chiSoBoTest) {
+        System.out.println("Test "+(chiSoBoTest+1)+":");
+        for (int i = 0; i < listCap.size(); i++) {
+            System.out.println(listCap.get(i).getSoCanXet()+" xuat hien "+listCap.get(i).getLanXuatHien()+" lan");
+        }
+    }
+}
+
+public class J02007 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        new XuLyBaiToanJ02007().thucThi(sc);
+        sc.close();
+    }
+}
